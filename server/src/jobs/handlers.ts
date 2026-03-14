7f5d9d20c2844c5cfc37e74bc7abcb1665d9836a
vconuepan <venancio@conuepan.cl>
2026-03-14 18:41:31 -0300

    Update handlers.ts

--- server/src/jobs/handlers.ts
+++ server/src/jobs/handlers.ts
@@ -9,6 +9,7 @@ import { runSocialAutoPost } from './socialAutoPost.js'
 import { runMastodonUpdateMetrics } from './mastodonUpdateMetrics.js'
 import { runSendNewsletter } from './sendNewsletter.js'
 import { runSendPrivateNewsletter } from './sendPrivateNewsletter.js'
+import { runScrapeDOCIP } from './scrapeDOCIP.js'
 
 export const JOB_HANDLERS: Record<string, () => Promise<void>> = {
   crawl_feeds: runCrawlFeeds,
@@ -22,4 +23,5 @@ export const JOB_HANDLERS: Record<string, () => Promise<void>> = {
   mastodon_update_metrics: runMastodonUpdateMetrics,
   send_newsletter: runSendNewsletter,
   send_private_newsletter: runSendPrivateNewsletter,
+  scrape_docip: runScrapeDOCIP,
 }
